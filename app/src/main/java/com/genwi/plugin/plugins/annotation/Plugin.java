92d27821594177f3c534b8c59de589833152cf63
sathis <sathis@genwi.com>
2016-03-15 19:54:58 +0530

    Fix:
    Retain Plugin annotation.

--- app/src/main/java/com/genwi/plugin/plugins/annotation/Plugin.java
+++ app/src/main/java/com/genwi/plugin/plugins/annotation/Plugin.java
@@ -4,12 +4,16 @@ import android.support.annotation.NonNull;
 
 import org.atteo.classindex.IndexAnnotated;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Author: sathis
  * Last Updated: 09/03/16.
  */
 
 @IndexAnnotated
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Plugin {
     @NonNull String type();
 }
